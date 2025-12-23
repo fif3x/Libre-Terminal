260b2b196817f859c07e369b78eefce920f3d9bf
fif3x <185714407+fif3x@users.noreply.github.com>
2025-12-24 00:51:08 +0200

    Refactor build script to organize output files
    Organize build output into bin/linux directory and set permissions.

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd ..
 g++ -I../include src/main/os.cpp src/main/main.cpp src/main/vars.cpp src/main/readconf.cpp src/main/log.cpp -o libreterminal
+mkdir -p bin
 mv libreterminal bin/
 cd bin
+mkdir -p linux
 mv libreterminal linux/
 cd linux
 chmod +rwx libreterminal
