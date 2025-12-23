b04b06f43535eb6e9233b688dae811037a4b5374
fif3x <185714407+fif3x@users.noreply.github.com>
2025-12-24 00:53:29 +0200

    Update build.bat to create directories before moving
    Added directory creation for 'bin' and 'win' before moving executable.

--- build/build.bat
+++ build/build.bat
@@ -1,9 +1,13 @@
 @echo off
 cd ..
 g++ src/main/os.cpp src/main/main.cpp src/main/vars.cpp -o libreterminal
+mkdir bin
 move libreterminal.exe bin/
 cd bin
+mkdir win
 move libreterminal.exe win/
 cd win
 libreterminal.exe
+
 cd ../..
+:: Please run this through the cmd, not PowerShell
