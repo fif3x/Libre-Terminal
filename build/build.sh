ae3ac040851fd57721fb722c80d3434443f311e5
fif3x <fif3x@disroot.org>
2026-01-15 22:02:36 +0200

    Added 'show_os' command, added config file in 'etc' folder, updated installation guide, added autobuild.sh script, updated build.sh script

--- build/build.sh
+++ build/build.sh
@@ -5,6 +5,8 @@ mkdir -p bin
 mv libreterminal bin
 make clean
 
+mkdir -p ~/.config/libreterminal && cp ./etc/config.conf ~/.config/libreterminal/config.conf
+
 sudo cp bin/libreterminal /usr/bin
 libreterminal
 
